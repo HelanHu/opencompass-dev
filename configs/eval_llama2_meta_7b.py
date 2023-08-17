2104fa1b58f499ccf16f5c6fa17ba0cf6d2b21e0
HelanHu <huhelan@stu.pku.edu.cn>
2023-08-17 17:59:01 +0800

    add llama meta

---
+++ configs/eval_llama2_meta_7b.py
@@ -0,0 +1,13 @@
+from mmengine.config import read_base
+
+with read_base():
+    from .models.llama2_7b_hhl import models
+
+    from .datasets.humaneval.humaneval_gen import humaneval_datasets
+datasets = [*humaneval_datasets]
+
+#     from .datasets.mmlu.mmlu_gen import mmlu_datasets
+# datasets = [*mmlu_datasets]
+
+
+# from opencompass.models import HuggingFaceCausalLM
